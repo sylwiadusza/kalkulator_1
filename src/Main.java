640b60d646ff6864faf01394dd318d57f7a8041e
sylwia <sylwiadusza280@gmail.com>
2022-11-23 20:56:46 +0100

    prostyKalkulator

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Witaj w kalkulatorze \nPodaj pierwszą cyfrę?");
+        double number1 = scanner.nextDouble();
+        System.out.println("Podaj drugą cyfrę?");
+        double number2 = scanner.nextDouble();
+        System.out.println("Możliwe opcje: \n1 - dodawanie \n2 - odejmowanie \n3 - mnozenie \n4 - dzielenie");
+        System.out.println("Wybierz opcję:");
+        int opcja = scanner.nextInt();
+        switch (opcja) {
+            case 1:
+                System.out.println(number1 + number2);
+                break;
+            case 2:
+                System.out.println(number1 - number2);
+                break;
+            case 3:
+                System.out.println(number1 * number2);
+                break;
+            case 4:
+                System.out.println(number1 / number2);
+                break;
+        }
+    }
+}
